cbf07fce09b990c8ff60f935242e51ee5287580f
tmdalsy <tmdalsy123@gmail.com>
2021-01-19 15:03:30 +0900

    commit test, 문자열 1번 아스키코드

---
+++ baekjoon/step7_string/num1_아스키코드.java
@@ -0,0 +1,14 @@
+package baekjoon.step7_string;
+
+import java.util.Scanner;
+
+public class num1_아스키코드 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		char c = scanner.next().charAt(0);
+		System.out.println((int)c);
+	}
+
+}
